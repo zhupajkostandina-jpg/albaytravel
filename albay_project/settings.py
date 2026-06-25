6d41e097431ee8710790a9e73bf248113109a3be
zhupajkostandina-jpg <zhupajkostandina@gmail.com>
2026-06-25 14:41:02 +0200

    fix database config

--- albay_project/settings.py
+++ albay_project/settings.py
@@ -80,13 +80,20 @@ WSGI_APPLICATION = 'albay_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
