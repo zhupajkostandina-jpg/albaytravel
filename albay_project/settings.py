632e8cd33eaa0ad4c39b6af9870e0c103313a282
zhupajkostandina-jpg <zhupajkostandina@gmail.com>
2026-07-02 19:34:50 +0200

    Restore production settings

--- albay_project/settings.py
+++ albay_project/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'albay_travel' / 'static',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
