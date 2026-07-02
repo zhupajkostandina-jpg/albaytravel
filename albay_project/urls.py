a9aa61f90b4450c1b2294939012ffee62fadfdc1
zhupajkostandina-jpg <zhupajkostandina@gmail.com>
2026-07-02 19:39:19 +0200

    Fix media serving on live

--- albay_project/urls.py
+++ albay_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('albay_travel.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
